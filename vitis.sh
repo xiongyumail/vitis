95877b5dc7219a7225de6d97c00afb855f3d3d86
XiongYu <xiongyu@espressif.com>
2021-06-11 14:46:43 +0800

    add JTAG driver install

--- vitis.sh
+++ vitis.sh
@@ -6,6 +6,9 @@ WORK_PATH=$(cd $(dirname $0); pwd)
 
 if [[ "$(sudo docker images -q "${IMAGE_NAME}:${IMAGE_VERSION}" 2> /dev/null)" == "" ]]; then
   ${WORK_PATH}/tools/gdocker/gdocker.sh install -n ${IMAGE_NAME} -v ${IMAGE_VERSION} -t ${WORK_PATH}/tools/tools.sh
+  cd ${WORK_PATH}/.tmp/vitis/vitis/Xilinx/Vivado/2020.1/data/xicom/cable_drivers/lin64/install_script/install_drivers
+  sudo ./install_drivers
+  cd ${WORK_PATH}
 fi
 
 if [[ "$1" == "" ]]; then
